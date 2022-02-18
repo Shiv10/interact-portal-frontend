86f0318970c5d47a6ad688241c3de824c67199b1
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-19 01:28:52 +0530

    Updates

---
+++ precache-manifest.1dd7845f690d2ae5570a4089458de01f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983f32d655950213ca4e8eb688239919",
+    "url": "/interactPortal/index.html"
+  },
+  {
+    "revision": "8c3e544a5ab40a84b341",
+    "url": "/interactPortal/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/interactPortal/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "8c3e544a5ab40a84b341",
+    "url": "/interactPortal/static/js/main.9b698de2.chunk.js"
+  },
+  {
+    "revision": "d4460ad16c82a23d5f0b",
+    "url": "/interactPortal/static/js/runtime-main.3db145c6.js"
+  }
+]);
