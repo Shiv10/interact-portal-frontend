0349a6947976d355bbbb73221600ee7cca8367e6
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-19 04:40:57 +0530

    feat: add functionality to add joke

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import abi from './utils/WaveFolder.json';
@@ -7,8 +7,10 @@ export default function App() {
 
   const [currentAccount, setCurrentAccount] = useState("");
   const [mining, setMining] = useState(false);
+  const [allJokes, setAllJokes] = useState([]);
+  const currentJoke = useRef();
   const [jokes, setJokes] = useState(0);
-  const contractAddress = "0x06EB61f4781fFB986E6d052b59c87bbdCb6424e5";
+  const contractAddress = "0x1Ae4462705F2c54aADDF6c62f1254efc452BF38A";
   const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
@@ -36,7 +38,7 @@ export default function App() {
     }
   }
 
-  const getJokeNumber =  async () => {
+  const getAllJokes =  async () => {
     try {
       const { ethereum } = window;
 
@@ -45,7 +47,17 @@ export default function App() {
         const signer = provider.getSigner();
         const jokePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
         let count = await jokePortalContract.getTotalJokes();
+        let jokes = await jokePortalContract.getAllJokes();
         setJokes(count.toNumber());
+        let jokesCleaned = [];
+        jokes.forEach(joke => {
+          jokesCleaned.push({
+            address: joke.joker,
+            timestamp: new Date(joke.timestamp * 1000),
+            message: joke.message
+          });
+        });
+        setAllJokes(jokesCleaned);
         console.log("retrived total count from contract: ", count.toNumber());
       } else {
         console.log("Ethereum object doesn't exist");
@@ -75,16 +87,24 @@ export default function App() {
     try {
       const { ethereum } = window;
 
+      const text = currentJoke.current.value;
+      
+      if(text==="") {
+        alert("Enter a joke.");
+        return;
+      }
+
       if(ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const jokePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
         
-        const jokeTxn = await jokePortalContract.joke();
+        const jokeTxn = await jokePortalContract.joke(text);
         setMining(true);
         await jokeTxn.wait();
         setMining(false);
         console.log(jokeTxn.hash);
+        alert("saved your joke!");
       } else {
         console.log("Ethereum object doesn't exist");
       }
@@ -109,10 +129,12 @@ export default function App() {
         I'm Shivansh. Tell me something funny and you might win a few ETH 👀!
         </div>
 
-        <button className="waveButton" onClick={getJokeNumber}>
-          Read number of jokes
+        <button className="waveButton" onClick={getAllJokes}>
+          See all jokes
         </button>
 
+        <br/>
+        <input ref={currentJoke} placeholder="Make me laugh!"></input>
         <button className="waveButton" onClick={joke} disabled={mining}>
           Crack a joke!
         </button>
@@ -126,6 +148,15 @@ export default function App() {
             </button>
           )
         }
+
+        {allJokes.map((joke, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {joke.address}</div>
+              <div>Time: {joke.timestamp.toString()}</div>
+              <div>Message: {joke.message}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
